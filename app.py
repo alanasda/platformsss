4b01123f73effc34d66c59085290b33fe4461b58
alanasda <ferreiramateuss000@gmail.com>
2025-04-07 15:20:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,6 @@ def webhook():
 def ping():
     return "✅ Online", 200
 
-if __name__ == '__main__':
-    app.run(port=5000)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
